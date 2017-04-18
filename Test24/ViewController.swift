496fed32587cadcee6afe2fe38538347d14e988d
Rahul Rakesh <rahulrakesh@gmail.com>
2017-04-18 12:43:38 +0530

    Initial Commit

---
+++ Test24/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test24
+//
+//  Created by Rahul Rakesh on 4/18/17.
+//  Copyright © 2017 Vulpe.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
